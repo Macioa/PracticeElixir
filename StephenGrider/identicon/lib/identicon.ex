cd0ea0924701b1d8540455c0921ce75124fa7567
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-08-18 12:34:10 -0500

    refactored pick color

--- StephenGrider/identicon/lib/identicon.ex
+++ StephenGrider/identicon/lib/identicon.ex
@@ -5,15 +5,18 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+    |> build_grid
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
     hex
     |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1) #arg = reference to mirror_row function, use instance of mirror_row that takes 1 argument
   end
 
-  def pick_color(%Identicon.Image{hex: hex_list} = image) do
-    %Identicon.Image{image | color: {r, g, b}}
+  def pick_color([r,g,b| tail]=image) do
+    %Identicon.Image{ hex: tail, color: {r,g,b} }
   end
 
   def mirror_row(row) do
@@ -22,7 +25,7 @@ defmodule Identicon do
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
   end
 
