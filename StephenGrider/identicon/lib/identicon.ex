ccc934e5cfa35f74f1fe5ff499df7f51f0639b7e
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-08-18 11:11:25 -0500

    pattern match struct

--- StephenGrider/identicon/lib/identicon.ex
+++ StephenGrider/identicon/lib/identicon.ex
@@ -7,8 +7,18 @@ defmodule Identicon do
     |> hash_input
   end
 
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+  end
+
   def pick_color(%Identicon.Image{hex: hex_list} = image) do
     %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 
   def hash_input(input) do
