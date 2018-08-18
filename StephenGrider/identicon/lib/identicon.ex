4d4ebb28e79e225ce1550b2b61a62784b2df556e
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-08-18 15:26:18 -0500

    egd and draw_image

--- StephenGrider/identicon/lib/identicon.ex
+++ StephenGrider/identicon/lib/identicon.ex
@@ -7,7 +7,34 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
   end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill_color = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill_color)
+    end
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    Enum.map grid, fn({_value, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+    %Identicon.Image{ image | pixel_map: [top_left, bottom_right] }
+    end
+  end
+
     @doc """
     Using image hex, break into 3 columns, mirror each row, then return a single list with indexes
   """
@@ -21,7 +48,7 @@ defmodule Identicon do
     %Identicon.Image{image | grid: grid}
   end
 
-  def filter_odd_square(%Identicon.Image{grid: grid}=image) do
+  def filter_odd_squares(%Identicon.Image{grid: grid}=image) do
     Enum.filter grid, fn({value, _index})->
       rem(value,2)
     end
@@ -29,7 +56,7 @@ defmodule Identicon do
   end
 
   def pick_color([r,g,b| tail]=image) do
-    %Identicon.Image{ hex: tail, color: {r,g,b} }
+    %Identicon.Image{ image | hex: tail, color: {r,g,b} }
   end
 
   def mirror_row(row) do
