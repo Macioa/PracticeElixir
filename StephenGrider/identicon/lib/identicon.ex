7be72638b730353ee3f5430b69cd199018c0383b
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-08-19 13:01:38 -0500

    edits

--- StephenGrider/identicon/lib/identicon.ex
+++ StephenGrider/identicon/lib/identicon.ex
@@ -4,13 +4,13 @@ defmodule Identicon do
     """
   def main(input) do
     input
-    |> hash_input
-    |> pick_color
-    |> build_grid
-    |> filter_odd_squares
-    |> build_pixel_map
-    |> draw_image
-    |> save_image(input)
+      |>hash_input
+      |>pick_color
+      |>build_grid
+      |>filter_odd_squares
+      |>build_pixel_map
+      |>draw_image
+      |>save_image(input)
   end
 
   def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
@@ -29,19 +29,50 @@ defmodule Identicon do
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
-    Enum.map grid, fn({_value, index}) ->
+    pixel_map = Enum.map grid, fn({_value, index}) ->
       horizontal = rem(index, 5) * 50
       vertical = div(index, 5) * 50
 
       top_left = {horizontal, vertical}
       bottom_right = {horizontal + 50, vertical + 50}
 
-    %Identicon.Image{ image | pixel_map: [top_left, bottom_right] }
+      {top_left, bottom_right}
     end
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
     @doc """
-          Using image hex, break into 3 columns, mirror each row, then return a single list with indexes
+    From image hex, break into 3 columns, mirror each row, then return a single list with indexes
+    ## Examples
+      iex> id = Identicon.pick_color([164, 140, 170, 144, 146, 152, 141, 117, 180, 193, 241, 190, 132, 212, 64, 88])
+      iex> Identicon.build_grid(id)
+      %Identicon.Image{
+        color: {164, 140, 170},
+        grid: [
+          {144, 0},
+          {146, 1},
+          {152, 2},
+          {146, 3},
+          {144, 4},
+          {141, 5},
+          {117, 6},
+          {180, 7},
+          {117, 8},
+          {141, 9},
+          {193, 10},
+          {241, 11},
+          {190, 12},
+          {241, 13},
+          {193, 14},
+          {132, 15},
+          {212, 16},
+          {64, 17},
+          {212, 18},
+          {132, 19}
+        ],
+        hex: [144, 146, 152, 141, 117, 180, 193, 241, 190, 132, 212, 64, 88],
+        pixel_map: nil
+      }
     """
   def build_grid(%Identicon.Image{hex: hex} = image) do
     grid =
@@ -54,8 +85,8 @@ defmodule Identicon do
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid}=image) do
-    Enum.filter grid, fn({value, _index})->
-      rem(value,2)
+    grid = Enum.filter grid, fn({value, _index})->
+      rem(value,2) == 0
     end
     %Identicon.Image{image | grid: grid}
   end
@@ -93,21 +124,4 @@ defmodule Identicon do
     |> :binary.bin_to_list
   end
 
-
-
-
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
-
-  def hello do
-    :world
-  end
 end
