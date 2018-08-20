7ebef3c45921dabde50e7daf6ea38b468e9ea316
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-08-20 17:13:01 -0500

    ecto sql orm

--- StephenGrider/discuss/web/controllers/topic_controller.ex
+++ StephenGrider/discuss/web/controllers/topic_controller.ex
@@ -15,4 +15,17 @@ defmodule Discuss.TopicController do
 
     render connection, "new.html", changeset: changeset
   end
+
+  def create(conn,  params) do
+    IO.inspect(params)
+    %{"topic"=>topic} = params
+
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> IO.inspect(changeset)
+    end
+  end
+
 end
