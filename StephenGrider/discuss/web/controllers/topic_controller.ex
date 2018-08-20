72d8db66bb33213797999e467ab57e50cadf55fb
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-08-20 12:32:39 -0500

    use alias

--- StephenGrider/discuss/web/controllers/topic_controller.ex
+++ StephenGrider/discuss/web/controllers/topic_controller.ex
@@ -1,10 +1,18 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
-  def new(req, params) do
-    IO.puts "++++"
-    IO.inspect req #print entire data structure for request connection
-    IO.puts "++++"
-    IO.inspect params
-    IO.puts "++++"
+  # def new(req, params) do
+  #   IO.puts "++++"
+  #   IO.inspect req #print entire data structure for request connection
+  #   IO.puts "++++"
+  #   IO.inspect params
+  #   IO.puts "++++"
+  # end
+
+  alias Discuss.Topic
+
+  def new(connection, params) do
+    struct = %Topic{}
+    params = %{}
+    changeset = Topic.changeset(struct, params)
   end
 end
